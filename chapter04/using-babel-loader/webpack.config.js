a4af9edd63dae1744571a174b5ee423db60896d1
Reiko Makimoto <gracesystems2016@gmail.com>
2020-05-02 18:52:41 +0900

    webpack.config.jsの別の書き方を追記

--- chapter04/using-babel-loader/webpack.config.js
+++ chapter04/using-babel-loader/webpack.config.js
@@ -12,26 +12,92 @@ module.exports = {
     // 出力するファイル名
     filename: 'bundle.js',
     // 出力先のパス（絶対パスを指定しないとエラーが出るので注意）
-    path: path.resolve(__dirname, 'public/js')
+    path: path.resolve(__dirname, 'public/js'),
   },
+  // module: {
+  //   rules: [
+  //     {
+  //       // ローダーの処理対象ファイル
+  //       test: /\.js$/,
+  //       // ローダーの処理対象となるディレクトリ
+  //       include: path.resolve(__dirname, 'src/js'),
+  //       use: [
+  //         {
+  //           // 利用するローダー
+  //           loader: 'babel-loader',
+  //           // ローダーのオプション
+  //           options: {
+  //             presets: [['@babel/preset-env', { modules: false }]],
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  //
+  // 別の書き方1<==うまくいかない
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js $/,
+  //       exclude: /node_ modules/,
+  //       use: [
+  //         {
+  //           loader: 'babel-loader?{ "presets":[["@babel/env", {"modules": false}]] }',
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // 別の書き方2
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: [
+  //         {
+  //           loader: 'babel-loader',
+  //           options: {
+  //             presets: [['@babel/preset-env', { modules: false }]],
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
+  // 利用するローダーが１つだけであれば、useを省略できる
   module: {
     rules: [
       {
-        // ローダーの処理対象ファイル
         test: /\.js$/,
-        // ローダーの処理対象となるディレクトリ
-        include: path.resolve(__dirname, 'src/js'),
-        use: [
-          {
-            // 利用するローダー
-            loader: 'babel-loader',
-            // ローダーのオプション
-            options: {
-              presets: [['@babel/preset-env', { modules: false }]]
-            }
-          }
-        ]
-      }
-    ]
-  }
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env', { modules: false }]],
+        },
+      },
+    ],
+  },
+  //
+  //複数のローダーを指定するときはuseを使う
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.js$/,
+  //       exclude: /node_modules/,
+  //       use: [
+  //         {
+  //           loader: 'babel-loader',
+  //           options: {
+  //             presets: [['@babel/preset-env', { modules: false }]],
+  //           },
+  //         },
+  //         {
+  //           loader: 'jshint-loader',
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
 };
